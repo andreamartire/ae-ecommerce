aae3b82114b7f2fefed47f92866d02efe7a3e32a
deadlyomen17 <deadlyomen17@836f44f4-8a78-9814-f7e0-9f8b9fac0099>
2012-02-13 14:38:38 +0000

    gestione prodotti alpha

--- src/aeecommerce/web/ProdottiController.java
+++ src/aeecommerce/web/ProdottiController.java
@@ -1,9 +1,13 @@
 package aeecommerce.web;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,14 +46,58 @@ public class ProdottiController {
 		return list;
 	}
 	
+	@RequestMapping(value = "/gestioneProdotti.htm", method = RequestMethod.GET)
+	public String gestioneProdotti(@RequestParam int idCategoria, ModelMap model) {
+		
+		List<Prodotto> prodotti = prodService.list();
+		List<Prodotto> list = new ArrayList<Prodotto>();
+		
+		for (Prodotto p : prodotti) {
+			if (p.getCategoria().getId() == idCategoria) 
+				list.add(p);
+		}
+		
+		model.put("categoria", catService.findById(idCategoria));
+		model.put("prodotti", list);
+		
+		return "productsManagement";
+	}
+	
 	@RequestMapping(value = "/aggiungiProdotto.htm", method = RequestMethod.POST)
-	public @ResponseBody String aggiungiProdotto(@RequestParam int idCategoria, @RequestParam String nome) {
+	public @ResponseBody String aggiungiProdotto(HttpServletRequest model) {
+		
+		System.out.println("aggiungo " + model.getParameter("nome") + " a " + model.getParameter("idCategoria")); 
 		
 		Prodotto p = new Prodotto();
-		p.setNome(nome);
-		Categoria c = catService.findById(idCategoria);
+		p.setNome((String) model.getParameter("nome"));
+		p.setPercentualeIVA(Double.parseDouble(model.getParameter("iva")));
+		p.setPesoApprossimato(Integer.parseInt(model.getParameter("peso")));
+		p.setPrezzoUnitario(Double.parseDouble(model.getParameter("prezzo")));
+		p.setDescrizione(model.getParameter("descrizione"));
+		Categoria c = catService.findById(Integer.parseInt(model.getParameter("idCategoria")));
 		p.setCategoria(c);
 		prodService.insert(p);
+		
+		return "Prodotto " + model.getParameter("nome") + " aggiunto alla categoria " + model.getParameter("idCategoria");
+	}
+	
+	@RequestMapping(value = "/eliminaProdotto.htm", method = RequestMethod.POST)
+	public @ResponseBody String eliminaProdotto(@RequestParam int idCategoria, @RequestParam int idProdotto) {
+		
+		Prodotto p = prodService.findById(idProdotto);
+		prodService.delete(p);
+		
+		return "Prodotto " + idProdotto + " eliminato dalla categoria " + idCategoria;
+	}
+	
+	@RequestMapping(value = "/modificaProdotto.htm", method = RequestMethod.POST)
+	public @ResponseBody String modificaProdotto(@RequestParam int idCategoria, @RequestParam int idProdotto) {
+		
+		Prodotto p = prodService.findById(idProdotto);
+		
+		// TODO set nuovi dati
+		
+		prodService.update(p);
 		
 		return "ok";
 	}
