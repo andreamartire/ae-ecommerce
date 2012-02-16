e37a84e262efc8db446f588fab1948b2083211c5
andreamartire <andreamartire@836f44f4-8a78-9814-f7e0-9f8b9fac0099>
2012-02-16 10:50:42 +0000

    gestione banner ready

--- src/aeecommerce/web/InfoController.java
+++ src/aeecommerce/web/InfoController.java
@@ -1,24 +1,65 @@
 package aeecommerce.web;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import javax.servlet.ServletContext;
+
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 
+import aeecommerce.pojo.Amministratore;
 import aeecommerce.service.UserService;
 
 
 @Controller
-public class InfoController {
-	
+@RequestMapping(value = "/gestioneInfo.htm")
+public class InfoController
+{
 	@Autowired
 	UserService userService;
 	
-	@RequestMapping(value={"/gestioneInfoOLD.htm"}, method = RequestMethod.GET)
-	public String getInfo(@ModelAttribute("user") String user)
+	@Autowired
+	ServletContext servletContext;
+
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String getUploadForm(Model model)
 	{
-		System.out.println("Gestione Info get user " + user);
-		return "adminInfo";
+		System.out.println("Upload get");
+		model.addAttribute(new UploadItem());
+		return "bannerEditor";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String create(UploadItem uploadItem, BindingResult result)
+	{
+		if (result.hasErrors())
+		{
+			for(ObjectError error : result.getAllErrors())
+			{
+				System.err.println("Error: " + error.getCode() +  " - " + error.getDefaultMessage());
+			}
+			return "bannerEditor/uploadForm";
+		}
+
+		try {
+			RandomAccessFile file = new RandomAccessFile(
+					new File(servletContext.getRealPath("/") + "resources/images/banner.jpg"), "rw");
+			file.write(uploadItem.getFileData().getBytes());
+			file.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return "redirect:home.htm";
 	}
 }
