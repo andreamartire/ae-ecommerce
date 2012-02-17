1815e3c6bf295b69fdc2994061157222475ef21b
andreamartire <andreamartire@836f44f4-8a78-9814-f7e0-9f8b9fac0099>
2012-02-17 15:28:19 +0000

    maps

--- branches/ae-ecommerce/src/aeecommerce/pojo/Document.java
+++ branches/ae-ecommerce/src/aeecommerce/pojo/Document.java
@@ -27,7 +27,7 @@ public class Document {
 	public Document() {
 		this.faq = Hibernate.createClob("FAQ non specificate");
 		this.condizioni = Hibernate.createClob("Condizioni non specificate");
-		this.doveSiamo = Hibernate.createClob("DoveSiamo non specificato");
+		this.doveSiamo = Hibernate.createClob("<p style=\"display:inline-block; vertical-align:middle; margin:0px auto; text-align:center;\"><iframe width=\"425\" height=\"350\" frameborder=\"0\" scrolling=\"no\" marginheight=\"0\" marginwidth=\"0\" src=\"http://maps.google.it/maps?f=q&amp;source=s_q&amp;hl=it&amp;geocode=&amp;q=Universit%C3%A0+della+Calabria,+Via+Pietro+Bucci,+Rende&amp;aq=0&amp;oq=universit%C3%A0+della&amp;sll=41.442726,12.392578&amp;sspn=16.852427,39.506836&amp;ie=UTF8&amp;hq=Universit%C3%A0+della+Calabria,+Via+Pietro+Bucci,+Rende&amp;t=h&amp;ll=39.355604,16.226978&amp;spn=0.023228,0.036478&amp;z=14&amp;iwloc=A&amp;output=embed\"></iframe><br /><small><a href=\"http://maps.google.it/maps?f=q&amp;source=embed&amp;hl=it&amp;geocode=&amp;q=Universit%C3%A0+della+Calabria,+Via+Pietro+Bucci,+Rende&amp;aq=0&amp;oq=universit%C3%A0+della&amp;sll=41.442726,12.392578&amp;sspn=16.852427,39.506836&amp;ie=UTF8&amp;hq=Universit%C3%A0+della+Calabria,+Via+Pietro+Bucci,+Rende&amp;t=h&amp;ll=39.355604,16.226978&amp;spn=0.023228,0.036478&amp;z=14&amp;iwloc=A\" style=\"color:#0000FF;text-align:left\">Visualizzazione ingrandita della mappa</a></small></p>");
 		this.contattaci = Hibernate.createClob("Contatti non specificati");
 	}
 
